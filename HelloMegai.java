c039368def608b757bc040cb211a034233e76422
Megaya Jesu <73550590+mrmegaya@users.noreply.github.com>
2021-04-09 18:31:48 +0530

    Update HelloMegai.java

--- HelloMegai.java
+++ HelloMegai.java
@@ -1,5 +1,6 @@
 class HelloMegai {
     public static void main(String[] args) {
         System.out.println("Hello, Megaya!"); 
+        System.out.println("Megaya! This has been working");
     }
 }
